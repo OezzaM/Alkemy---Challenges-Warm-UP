3d44f38f467783e2eb27bf406417d8666998945b
Octavio <octaviomazzeo@gmail.com>
2020-12-21 15:04:35 -0300

    Endpoint utilization consuming API

--- frontend/src/context/postReducer.js
+++ frontend/src/context/postReducer.js
@@ -1,17 +1,49 @@
-import { FORMULARIO_OPERACION, OBTENER_POSTS } from '../types'
+import { SHOW_FORMULARIO, GET_POSTS, CURRENT_POST, CREATE_POST, UPDATE_POST, DELETE_POST, GET_POST, POST_NULL } from '../types'
 
 const PostReducer = (state, action) => {
     
     switch (action.type){
-        case FORMULARIO_OPERACION:
+        case SHOW_FORMULARIO:
             return {
                 ...state,
                 formulario: action.payload
             }
-         case OBTENER_POSTS: 
+         case GET_POSTS: 
             return {
                 ...state,
                 posts: action.payload
+            }
+         case CURRENT_POST:
+            return {
+                ...state,
+                _currentPost: action.payload
+            }
+         case CREATE_POST:
+            return {
+                ...state,
+                posts: [...state.posts, action.payload]
+            }
+         case UPDATE_POST:
+            return {
+                ...state,
+                posts: state.posts.map(post => post._id === action.payload._id ? action.payload : post )
+            }
+        case DELETE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== action.payload),
+            }
+        case GET_POST:
+            return {
+                ...state,
+                post: action.payload,
+                isNull: false
+            }
+        case POST_NULL:
+            return {
+                ...state,
+                isNull: action.payload,
+                post: undefined
             }
     default: 
         return state;
--- frontend/src/context/postState.js
+++ frontend/src/context/postState.js
@@ -3,35 +3,98 @@ import React, { useReducer } from 'react';
 import PostContext from './postContext';
 import PostReducer from './postReducer';
 import clienteAxios from '../config/axios';
-import { FORMULARIO_OPERACION, OBTENER_POSTS } from '../types'
+import { SHOW_FORMULARIO, GET_POSTS, CURRENT_POST, CREATE_POST, UPDATE_POST, DELETE_POST, GET_POST, POST_NULL } from '../types'
 
 const PostState = props => {
 
     const initialState = {
         posts: [],
-        formulario : false
+        formulario : false,
+        _currentPost: null,
+        post: null,
+        isNull: false
     }
 
     const [state, dispatch] = useReducer(PostReducer, initialState);
 
 
-    const mostrarFormulario = (formulario) => {
+    const showFormulario = (formulario) => {
         dispatch({
-            type: FORMULARIO_OPERACION,
+            type: SHOW_FORMULARIO,
             payload: formulario
         })
     }
 
-    const obtenerPosts = async () => {
+    const getPosts = async () => {
         try {
             const resultado = await clienteAxios.get('https://jsonplaceholder.typicode.com/posts');
             
             dispatch({
-                type: OBTENER_POSTS,
+                type: GET_POSTS,
                 payload: resultado.data
             })
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    const currentPost = post => {
+        dispatch({
+            type: CURRENT_POST,
+            payload: post
+        })
+    }
+
+    const createPost = async post => {
+        try {
+            const resultado = await clienteAxios.post('https://jsonplaceholder.typicode.com/posts', post)
+        
+        dispatch({
+            type: CREATE_POST,
+            payload: resultado.data
+        })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const updatePost = async post => {
+        try {
+            const resultado = await clienteAxios.put(`https://jsonplaceholder.typicode.com/posts/${post.id}`, post);
+            
+            dispatch({
+                type: UPDATE_POST,
+                payload: resultado.data
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const deletePost = async postId => {
+        try {
+            await clienteAxios.delete(` https://jsonplaceholder.typicode.com/posts/${postId}`)
+            dispatch({
+                type: DELETE_POST,
+                payload: postId
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getPost = async postId => {
+        try {
+            const resultado = await clienteAxios.get(` https://jsonplaceholder.typicode.com/posts/${postId}`);
+            dispatch({
+                type: GET_POST,
+                payload: resultado.data
+            })
+        } catch (error) {
+            dispatch({
+                type: POST_NULL,
+                payload: true
+            })
         }
     }
 
@@ -41,8 +104,16 @@ const PostState = props => {
             value={{
                 posts: state.posts,
                 formulario: state.formulario,
-                mostrarFormulario,
-                obtenerPosts
+                _currentPost: state._currentPost,
+                post: state.post,
+                isNull: state.isNull,
+                showFormulario,
+                getPosts,
+                currentPost,
+                createPost,
+                updatePost,
+                deletePost,
+                getPost
             }}
         >
             {props.children}
